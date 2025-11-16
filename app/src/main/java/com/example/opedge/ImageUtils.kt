140b199bdca0476795a0f6b89d71c4577df3c06f
Anupama H N <137167751+Anupamaa-HN@users.noreply.github.com>
2025-11-16 12:45:25 +0530

    android: add ImageUtils for YUV image handling

---
+++ app/src/main/java/com/example/opedge/ImageUtils.kt
@@ -0,0 +1,26 @@
+package com.example.opedge
+
+import android.graphics.ImageFormat
+import android.media.Image
+import java.nio.ByteBuffer
+
+object ImageUtils {
+
+    fun yuv420ToByteArray(image: Image): ByteArray {
+        val yBuffer: ByteBuffer = image.planes[0].buffer
+        val uBuffer: ByteBuffer = image.planes[1].buffer
+        val vBuffer: ByteBuffer = image.planes[2].buffer
+
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + uSize + vSize)
+
+        yBuffer.get(nv21, 0, ySize)
+        vBuffer.get(nv21, ySize, vSize)
+        uBuffer.get(nv21, ySize + vSize, uSize)
+
+        return nv21
+    }
+}
